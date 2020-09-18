4d8d9390336d9b902f7c3b0b828c210d7a3a9489
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-09-18 17:14:48 +0900

    ユーザーと記事のリレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_035429) do
+ActiveRecord::Schema.define(version: 2020_09_13_002715) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
