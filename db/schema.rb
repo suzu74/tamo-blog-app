f650dc4bef7dac49083ce5d8e00ab17d5043c879
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-09-19 09:42:00 +0900

    comment function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_002715) do
+ActiveRecord::Schema.define(version: 2020_09_18_084018) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_09_13_002715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
