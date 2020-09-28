ce000ee6c70a25c6c7abe127267c870f1e1b49f2
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-09-28 18:58:35 +0900

    deploy関係

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TamoBlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    Bundler.require(*Rails.groups)
+    Dotenv::Railtie.load
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
