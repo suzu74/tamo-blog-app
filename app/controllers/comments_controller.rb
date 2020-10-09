d2b626b7da97b01508c80b0924b99cc2927fa554
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-10-09 09:59:49 +0900

    コメント機能をAjax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,18 @@ class CommentsController < ApplicationController
     @comment = article.comments.build
   end
 
+  def index
+    article = Article.find(params[:article_id])
+    comments = article.comments
+    render json: comments
+  end
+
   def create
     article = Article.find(params[:article_id])
     @comment = article.comments.build(comment_params)
+    @comment.save!
 
-    if @comment.save
-      redirect_to article_path(article), notice: 'コメントを追加しました'
-    else
-      flash.now[:error] = 'コメントできませんでした' 
-      render :new
-    end
+    render json: @comment
   end
 
   private
