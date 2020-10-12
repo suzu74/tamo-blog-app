ae052eb41f681e56749413cb6223917931be6cf7
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-10-12 09:49:01 +0900

    routingの修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,26 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   devise_for :users
+
   root to: 'articles#index'
-  resource :timeline, only: [:show]
 
-  resources :articles do
-    resources :comments, only: [:index, :new, :create]
-
-    resource :like, only: [:show, :create, :destroy]
-  end
+  resources :articles
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
     resources :unfollows, only: [:create]
   end
 
-  resource :profile, only: [:show, :edit, :update]
-  resources :favorites, only: [:index]
+  scope module: :apps do
+    resource :timeline, only: [:show]
+    resource :profile, only: [:show, :edit, :update]
+    resources :favorites, only: [:index] 
+  end
+
+  namespace :api, defaults: {format: :json} do
+    scope '/articles/:article_id' do
+      resources :comments, only: [:index, :create]
+      resource :like, only: [:show, :create, :destroy]
+    end
+  end
 end
